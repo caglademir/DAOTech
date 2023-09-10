1d149d45a492f53aed0cdec728d3fb2f8c3bf4ed
caglademir <caglllademir@gmail.com>
2023-09-10 14:49:37 +0300

    Debug

--- Front-end/src/Components/NavBar.jsx
+++ Front-end/src/Components/NavBar.jsx
@@ -54,15 +54,15 @@ export default function NavBar({ setIsOpen }) {
          
         </div>
         {/* <Wallet/> */}
-        <div id="button" className="flex items-center justify-center float-left">
+      </div>
+        <div id="button" className="flex items-center justify-center">
             <button 
                 
-                className="flex items-center bg-gradient-to-r from-blue-500 to-blue-800 hover:bg-gradient-to-bl border-blue-400 text-gray-200 font-antonio p-2 py-6 rounded-lg h-[15%] mr-10">
+                className="flex justify-center items-center bg-gradient-to-r from-blue-500 to-blue-800 hover:bg-gradient-to-bl border-blue-400 text-gray-200 font-antonio p-2 py-6 rounded-lg h-[15%] mr-10">
                 Connect to Phantom 
-                <img src="src/img/ghost.png" alt="phantom-logo" className="w-9 ml-2 md:invisible" />
+                <img src="src/img/ghost.png" alt="phantom-logo" className="w-9 ml-2 md:visible" />
             </button>
         </div>
-      </div>
 
      
     </div>
